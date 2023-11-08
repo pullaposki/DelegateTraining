24a269e40a485c5c825b7b7b81a0aa75974ba474
pullaposki <pullaposki@windowslive.com>
2023-11-08 12:29:38 +0200

    Add project files.

---
+++ DelegateTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateTraining
+{
+	internal class DelegateTest
+	{
+		//Create a delegate
+		public delegate void TestDelegate();
+
+		//Create a field of type TestDelegate
+		public TestDelegate DelegateField;
+
+		public void AssignFirstFunctionToField()
+		{
+			DelegateField = MyTestDelegateFunction;
+		}
+
+		public void AssingSecondFunctionToField()
+		{
+			DelegateField = MyTestDelegateFunction2;
+		}
+
+		public void AddFirstFunctionToField()
+		{
+			DelegateField += MyTestDelegateFunction;
+		}
+
+		public void AddSecondFunctionToField()
+		{ 
+			DelegateField += MyTestDelegateFunction2;
+		}
+
+		public void RemoveFirstFunctionFromField()
+		{
+			DelegateField -= MyTestDelegateFunction;
+		}
+
+		public void RemoveSecondFunctionFromField()
+		{
+			DelegateField -= MyTestDelegateFunction2;
+		}
+
+		void MyTestDelegateFunction()
+		{
+			Console.WriteLine("function 1 executed");
+		}
+
+		void MyTestDelegateFunction2()
+		{
+			Console.WriteLine("function 2 executed");
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DelegateTraining
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//https://www.youtube.com/watch?v=3ZfwqWl-YI0&ab_channel=CodeMonkey
+			//5:11
+
+			DelegateTest delegateTest = new DelegateTest();
+			
+			delegateTest.AssingSecondFunctionToField();
+			delegateTest.AddSecondFunctionToField();
+			delegateTest.RemoveSecondFunctionFromField();
+			delegateTest.DelegateField();
+		}
+	}
+}
